ccfb2b91a7b27813c41a952bb02700a867d4ad79
Hyaline <charles.eldwin@gmail.com>
2024-01-27 23:07:07 -0500

    adding all but stretch

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
 // import the pets array from data.js
+const exp = require('constants');
 const pets = require('./data');
 
 // init express app
-const express = require('express');
+const express = require('express')
+//import path
+const path = require("path");
+
+//this happens when you tab too fast before selecting require('express');
+//leaving this here for posterity
+// const { escape } = require('querystring');
+
 const app = express();
 
+
 const PORT = 8080;
+
+
+//adding static path for serving html/react app
+// this is a huge lift. not sure i will get to it.
+app.use(express.static(path.join(__dirname,"public")))
 
 // GET - / - returns homepage
 app.get('/', (req, res) => {
     // serve up the public folder as static index.html file
-
+    res.sendFile(__dirname + "/public/index.html")
 });
 
 // hello world route
@@ -21,30 +35,34 @@ app.get('/api', (req, res) => {
 // get all pets from the database
 app.get('/api/v1/pets', (req, res) => {
     // send the pets array as a response
-
+    res.send(pets)
 });
 
 // get pet by owner with query string
 app.get('/api/v1/pets/owner', (req, res) => {
     // get the owner from the request
-
+    const owner= req.query.owner;
 
     // find the pet in the pets array
-    const pet = pets.find(pet => pet.owner === owner);
+    const pet = pets.find(pet => pet.owner.toLowerCase() === owner.toLowerCase());
+    
 
     // send the pet as a response
+    res.send(pet)
 
 });
 
 // get pet by name
 app.get('/api/v1/pets/:name', (req, res) => {
     // get the name from the request
-
+    const name = req.query.name;
+   
 
     // find the pet in the pets array
-    const pet = pets.find(pet => pet.name === name);
+    const pet = pets.find(pet => pet.name.toLowerCase() === name.toLowerCase());
 
     // send the pet as a response
+    res.send(pet)
 
 });
 
